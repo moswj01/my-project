77b5f9ed5e975f99752f6e5d235c8e59103c88a3
Moss <watchara@centralsiam.com>
2022-08-20 03:38:07 +0700

    a

--- config/database.js
+++ config/database.js
@@ -1,11 +1,22 @@
-const path = require('path');
+// path: ./config/database.js
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env(
+        "DATABASE_HOST",
+        "wjtech-do-user-12243317-0.b.db.ondigitalocean.com"
+      ),
+      port: env.int("DATABASE_PORT", 25060),
+      database: env("DATABASE_NAME", "defaultdb"),
+      user: env("DATABASE_USERNAME", "doadmin"),
+      password: env("DATABASE_PASSWORD", "AVNS_LrnyweMDmQaUqR75h8B"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
