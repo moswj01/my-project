ee1d3cfd5cfa78ff89d3b44981cd45a30d4228ba
Moss <watchara@centralsiam.com>
2022-08-20 19:14:37 +0700

    pg

--- config/database.js
+++ config/database.js
@@ -4,17 +4,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env(
-        "DATABASE_HOST",
-        "wjtech-do-user-12243317-0.b.db.ondigitalocean.com"
-      ),
+      host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 25060),
       database: env("DATABASE_NAME", "defaultdb"),
       user: env("DATABASE_USERNAME", "doadmin"),
       password: env("DATABASE_PASSWORD", "AVNS_LrnyweMDmQaUqR75h8B"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        ca: env("DATABASE_CA"),
       },
     },
     debug: false,
