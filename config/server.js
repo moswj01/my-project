b5e4ef680a167db0a306aaa16c6f9ae6145e1f5a
Moss <watchara@centralsiam.com>
2022-08-21 00:30:01 +0700

    server

--- config/server.js
+++ config/server.js
@@ -3,10 +3,5 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
-    url: env("PUBLIC_URL", "https://api.wjtech.co/"),
-    proxy: env.bool("IS_PROXIED", true),
-    cron: {
-      enabled: env.bool("CRON_ENABLED", false),
-    },
   },
 });
